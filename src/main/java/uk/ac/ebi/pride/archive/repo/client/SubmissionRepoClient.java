4a125b3a2030cdbd4fdb10d7c3cb1ed29b1e5445
Chakradhar Bandla <chakrabandla@gmail.com>
2020-08-26 21:42:24 +0100

    submissionDto save fixes

--- src/main/java/uk/ac/ebi/pride/archive/repo/client/SubmissionRepoClient.java
+++ src/main/java/uk/ac/ebi/pride/archive/repo/client/SubmissionRepoClient.java
@@ -3,6 +3,7 @@ package uk.ac.ebi.pride.archive.repo.client;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import uk.ac.ebi.pride.archive.repo.client.utils.Utils;
+import uk.ac.ebi.pride.archive.repo.models.project.Project;
 import uk.ac.ebi.pride.archive.repo.models.submission.SubmissionDto;
 
 public class SubmissionRepoClient {
@@ -19,10 +20,15 @@ public class SubmissionRepoClient {
 
     public void save(SubmissionDto submissionDto) throws JsonProcessingException {
         final String url = SUBMISSION_URL_PATH + "/save";
-        submissionDto.getProject().setId(-1L);
+        Project project = submissionDto.getProject();
+        Long id = project.getId();
+        if (id == null) {
+            id = -1L;
+            project.setId(id);
+        }
         String payload = objectMapper.writeValueAsString(submissionDto);
-        payload = payload.replaceAll("\"project\":null", "\"project\":-1");
-        payload = payload.replaceAll("\"project\": null", "\"project\":-1");
+        payload = payload.replaceAll("\"project\":null", "\"project\":" + id);//to avoid null constraint checks for foreign keys
+        payload = payload.replaceAll("\"project\": null", "\"project\":" + id);
         prideRepoRestClient.sendPostRequest(url, payload);
     }
 }
